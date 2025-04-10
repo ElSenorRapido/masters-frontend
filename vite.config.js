621bbe1ded3f1b3cf7be0d28effecab48d100628
ElSenorRapido <stephenquick45@gmail.com>
2025-04-10 18:09:52 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Ensure Vite outputs to the correct directory
     rollupOptions: {
-      external: ['some-external-module']
+      input: '/index.html', // Set input to the right location for the entry HTML file
     }
   }
-});
+})
